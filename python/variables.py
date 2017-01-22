8abe298cee9507fc25b72339a74b36267169c574
srdunlap <dunlap.sarah@gmail.com>
2017-01-22 13:46:14 -0500

    python first lessons

---
+++ python/variables.py
@@ -0,0 +1,5 @@
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+print('Ariel is ' + str(age) + ' years old now, but she will turn ' + str(next_age) + ' in ' + (birth_month) + '. Her birth stone is '+ (birth_stone) + '.')
