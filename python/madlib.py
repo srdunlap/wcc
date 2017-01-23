67f770048b1769afe2676f769b2e836824833b14
srdunlap <dunlap.sarah@gmail.com>
2017-01-23 07:41:40 -0500

    python lesson 2

---
+++ python/madlib.py
@@ -0,0 +1,39 @@
+print('Welcome to the Mad Lib generator!')
+adjective1=(raw_input('Type in an adjective:'))
+adjective2=(raw_input('Type in another adjective:'))
+adjective3=(raw_input('And another adjective:'))
+adjective4=(raw_input('One more adjective:'))
+plural_animal=(raw_input('Plural animal (e.g., puppies):'))
+verb=(raw_input('Now a verb:'))
+plural_noun=(raw_input('Now a plural noun:'))
+name=(raw_input('The first name of a woman musician:'))
+beverage=(raw_input('A beverage:'))
+plural_body_part=(raw_input('And finally, a plural body part:'))
+
+#adjective1 = 'fearless'
+#adjective2 = 'gooey'
+#adjective3 = 'sassy'
+#adjective4 = 'fancy'
+
+#plural_animal = 'elephants'
+#verb = 'skip'
+#plural_noun = 'marbles'
+
+#name = 'Ani'
+#beverage = 'chocolate milk'
+#plural_body_part = 'knees'
+
+print('')
+print("Excellent choices! Here's your story")
+print('--------------------------------')
+print("There once was a " + adjective1 + " programmer named " + name)
+print("who wanted to build a " + adjective2 + " mobile app to ")
+print("help " + adjective3 + " " + plural_animal + " find new homes.")
+print(name + " stayed up all night, drinking lots of caffeinated " + beverage)
+print("as she worked and worked, trying to complete her " + adjective4 + " program.")
+print('')
+print("Whenever " + name + " hit a dead end, she would exclaim " + plural_noun + "!")
+print("But she was not discouraged, and after a quick break to " + verb + " and clear her head, she got back to work.")
+print('')
+print("By morning, when the sun started to rise, she let out a big yawn and stretched her " + plural_body_part + ". Finally, she was done.")
+print('--------------------------------')
